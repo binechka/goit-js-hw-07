1160d97f7418e97ea2af0b8166f7ab0e20f23534
Albina Bondar <albinabondar2019@gmail.com>
2022-08-09 23:18:49 +0300

    add exercises

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+gallery.innerHTML = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+).join("");
+
+
+gallery.addEventListener("click", onModalGallery);
+function onModalGallery(eve) {
+    eve.preventDefault();
+
+    document.addEventListener("keydown", removeEscape)
+
+    function removeEscape(even) { 
+        if (even.code !== "Escape") {
+            return
+        }
+        else {
+            instance.close()  
+            document.removeEventListener("keydown", removeEscape)
+        }
+    
+    }
+    const srcBigImages = eve.target.dataset.source;
+
+const instance = basicLightbox.create(`<img src="${srcBigImages}" width="800" height="600">`);
+
+instance.show()
+
+}
